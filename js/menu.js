5082b90b6e60f2f540ad37cb8a78c1c78e7bb6d6
Jonathan Darling <jmdarling@me.com>
2015-02-03 11:08:14 -0600

    Adds basis for responsive navigation.

--- js/menu.js
+++ js/menu.js
@@ -1,37 +1,55 @@
 (function () {
     "use strict";
 
-    var $menu;
+    var nav;
+    var $navMenu;
 
     $(document).ready(function() {
-        $menu = $('#menu');
+        $navMenu = $('#nav');
+        setupResponsiveNav();
         setupMenuItemClickHandlers();
     });
 
-    function setupMenuItemClickHandlers() {
-        $menu.find('.menu_item').click(menuItemClickHandler);
+    /**
+     * Setup the responsive-nav plugin.
+     */
+    function setupResponsiveNav() {
+        nav = responsiveNav("#nav", {
+            animate: true, // Boolean: Use CSS3 transitions, true or false
+            transition: 180, // Integer: Speed of the transition, in milliseconds
+            label: "SWAG", // String: Label for the navigation toggle
+            insert: "before", // String: Insert the toggle before or after the navigation
+            customToggle: "", // Selector: Specify the ID of a custom toggle
+            closeOnNavClick: true, // Boolean: Close the navigation when one of the links are clicked
+            openPos: "relative", // String: Position of the opened nav, relative or static
+            navClass: "nav-collapse", // String: Default CSS class. If changed, you need to edit the CSS too!
+            navActiveClass: "js-nav-active", // String: Class that is added to  element when nav is active
+            jsClass: "js", // String: 'JS enabled' class which is added to  element
+            init: function(){}, // Function: Init callback
+            open: function(){}, // Function: Open callback
+            close: function(){} // Function: Close callback
+        });
     }
 
+    /**
+     * Setup the menu item click handlers.
+     */
+    function setupMenuItemClickHandlers() {
+        $navMenu.find('li').click(menuItemClickHandler);
+    }
+
+    /**
+     * Menu item click handler.
+     *
+     * Applies the active css active class to the selected item and removes it
+     * from others.
+     */
     function menuItemClickHandler() {
-        highlightMenuItem(this.id);
-        switch (this.id) {
-            case 'about_me_button':
-                break;
+        var $this = $(this);
 
-            case 'projects_button':
-                break;
-
-            case 'resume_button':
-                break;
-
-            case 'contact_me_button':
-                break;
-        }
-
-        function highlightMenuItem(id) {
-            $menu.find('li.selected').removeClass('selected');
-            $menu.find('#' + id).addClass('selected');
-        }
+        // Apply the active class to the selected menu item.
+        $navMenu.find('li.active').removeClass('active');
+        $this.addClass('active');
     }
 
 })(jQuery);
