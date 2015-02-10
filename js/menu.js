172ebe5b55d231a17803aa0f3b6328d60258d910
Jonathan Darling <jmdarling@me.com>
2015-02-10 12:00:21 -0600

    Starts implementation of custom responsive menu.

--- js/menu.js
+++ js/menu.js
@@ -1,41 +1,18 @@
 (function () {
     "use strict";
 
-    var nav;
     var $navMenu;
 
     $(document).ready(function() {
-        $navMenu = $('#nav');
-        setupResponsiveNav();
+        $navMenu = $('#navigation-links');
         setupMenuItemClickHandlers();
     });
-
-    /**
-     * Setup the responsive-nav plugin.
-     */
-    function setupResponsiveNav() {
-        nav = responsiveNav("#nav", {
-            animate: true, // Boolean: Use CSS3 transitions, true or false
-            transition: 180, // Integer: Speed of the transition, in milliseconds
-            label: "SWAG", // String: Label for the navigation toggle
-            insert: "before", // String: Insert the toggle before or after the navigation
-            customToggle: "", // Selector: Specify the ID of a custom toggle
-            closeOnNavClick: true, // Boolean: Close the navigation when one of the links are clicked
-            openPos: "relative", // String: Position of the opened nav, relative or static
-            navClass: "nav-collapse", // String: Default CSS class. If changed, you need to edit the CSS too!
-            navActiveClass: "js-nav-active", // String: Class that is added to  element when nav is active
-            jsClass: "js", // String: 'JS enabled' class which is added to  element
-            init: function(){}, // Function: Init callback
-            open: function(){}, // Function: Open callback
-            close: function(){} // Function: Close callback
-        });
-    }
 
     /**
      * Setup the menu item click handlers.
      */
     function setupMenuItemClickHandlers() {
-        $navMenu.find('li').click(menuItemClickHandler);
+        $navMenu.find('a').click(menuItemClickHandler);
     }
 
     /**
@@ -48,7 +25,7 @@
         var $this = $(this);
 
         // Apply the active class to the selected menu item.
-        $navMenu.find('li.active').removeClass('active');
+        $navMenu.find('a.active').removeClass('active');
         $this.addClass('active');
     }
 
