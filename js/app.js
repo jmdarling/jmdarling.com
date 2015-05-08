6cae583cb30a6cd25b95d6560b558515031fb8f0
Jonathan Darling <jmdarling@me.com>
2015-05-07 22:10:42 -0500

    Small chart improvements.

--- js/app.js
+++ js/app.js
@@ -28,19 +28,20 @@
             datasets: [
                 {
                     label: 'Professional Experience',
-                    fillColor: 'rgba(255,44,97,0.8)',
+                    fillColor: 'rgba(255,44,97,1)',
                     strokeColor: 'rgba(255,44,97,1)',
                     highlightFill: 'rgba(255,44,97,0.9)',
                     highlightStroke: 'rgba(255,44,97,1)',
-                    // TODO: Dynamically calculate these.
                     data: [3, 2, 1, 2, 2, 2, 1, 2, 1, 3]
                 }
             ]
         };
 
         var options = {
+            responsive: true,
             scaleFontColor: 'rgba(238,238,238,0.8)',
             scaleFontFamily: '"Raleway", "Helvetica Neue", "Helvetica", "Arial", sans-serif',
+            scaleLabel: " <%=value%> years",
             scaleLineColor: 'rgba(238,238,238,0.2)',
             scaleGridLineColor: 'rgba(238,238,238,0.2)',
             tooltipFontFamily: '"Raleway", "Helvetica Neue", "Helvetica", "Arial", sans-serif'
